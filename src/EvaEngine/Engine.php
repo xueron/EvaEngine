4162b54caea40221a9cf651d38bab18dfc3c3bca
Xueron Nee <xueron@xueron.com>
2015-08-27 08:58:58 +0800

    write connection should not be shared
    write connection should not be shared

--- src/EvaEngine/Engine.php
+++ src/EvaEngine/Engine.php
@@ -570,7 +570,7 @@ class Engine
             function () use ($self) {
                 return $self->diDbMaster();
             },
-            true
+            false
         );
 
         $di->set(
